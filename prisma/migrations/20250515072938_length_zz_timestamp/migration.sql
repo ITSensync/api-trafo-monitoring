2bf9d2b86751ed29ef9e905746b104f230cdb6e3
Alief Muhammad Abdillah <aliefmabdillah09@gmail.com>
2025-05-15 14:29:55 +0700

    chore: length timestamp zz

---
+++ prisma/migrations/20250515072938_length_zz_timestamp/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `createdAt` on the `monitoring` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(6)` to `Timestamp(3)`.
+
+*/
+-- AlterTable
+ALTER TABLE `monitoring` MODIFY `createdAt` TIMESTAMP(3) NOT NULL;
