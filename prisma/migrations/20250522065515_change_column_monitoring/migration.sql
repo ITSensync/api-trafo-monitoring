d18249e19e4ff35f85054eb2c59e70e3b581d1c3
alief abdillah <aliefmabdillah09@gmail.com>
2025-05-22 14:01:57 +0700

    chore: seperate column suhu in db

---
+++ prisma/migrations/20250522065515_change_column_monitoring/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `suhu` on the `monitoring` table. All the data in the column will be lost.
+  - Added the required column `suhu_cpu` to the `Monitoring` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `suhu_trafo` to the `Monitoring` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `monitoring` DROP COLUMN `suhu`,
+    ADD COLUMN `suhu_cpu` DECIMAL(10, 1) NOT NULL,
+    ADD COLUMN `suhu_trafo` DECIMAL(10, 1) NOT NULL;
